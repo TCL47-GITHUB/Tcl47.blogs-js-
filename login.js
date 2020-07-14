5c198c5fb07aedc9ec533320dd6eaa1a42b1d696
TCL47-GITHUB <55310194+TCL47-GITHUB@users.noreply.github.com>
2020-07-14 11:52:22 +0700

    Add files via upload

---
+++ login.js
@@ -0,0 +1,13 @@
+<!--- Login /--> 
+<script language='javascript' type='text/javascript'> 
+function display(form){
+if (form.username.value==&quot;root&quot;) { 
+		if (form.password.value==&quot;root&quot;) {              
+										location=&quot;https://tcl47.blogspot.com/&quot; 
+											} else {
+													alert(&quot;Invalid Password&quot;)
+													}
+								} else {  alert(&quot;Invalid Username&quot;)
+										}
+						}
+			</script>			
